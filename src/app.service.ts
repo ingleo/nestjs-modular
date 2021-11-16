9ba9550053b6e3892f78881e569c4eef5b41d6dd
leonardo <10843115+ingleo@users.noreply.github.com>
2021-11-16 01:27:20 -0500

    added injection for value of global module

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,12 +8,13 @@ export class AppService {
     @Inject('OLD_API_KEY') private oldApiKey: any,
     @Inject('TASKS') private tasks: any[],
     @Inject(config.KEY) private configType: ConfigType<typeof config>,
+    @Inject('API_KEY_DB') private apiKeyDb: string,
     private configService: ConfigService,
   ) {}
   getHello(): string {
     console.log(this.tasks);
     const apiKey = this.configService.get<string>('API_KEY');
     const dbName = this.configType.database.name;
-    return `Hello World! ${this.oldApiKey.value} - ${apiKey} - ${dbName}- ${this.tasks[2].title}`;
+    return `Hello World! ${this.oldApiKey.value} - ${apiKey} - ${dbName}- ${this.tasks[2].title} - ${this.apiKeyDb}`;
   }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,16 +1,17 @@
 import { Module, Global } from '@nestjs/common';
 
-const API_KEY = '12345678';
-const API_KEY_PROD = '87654321';
+const API_KEY_DB = '12345678';
+const API_KEY_PROD_DB = '87654321';
 
 @Global()
 @Module({
   providers: [
     {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+      provide: 'API_KEY_DB',
+      useValue:
+        process.env.NODE_ENV === 'production' ? API_KEY_PROD_DB : API_KEY_DB,
     },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY_DB'],
 })
 export class DatabaseModule {}
